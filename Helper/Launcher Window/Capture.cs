d1c4eba4d46d4066c96f0402fedb290d8808264d
Pipe <pipernene@gmail.com>
2017-11-08 19:05:01 +0100

    Add PrintWindow method

--- Helper/Launcher Window/Capture.cs
+++ Helper/Launcher Window/Capture.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +11,25 @@ namespace Helper.Launcher_Window
 {
     internal static class Capture
     {
+        #region WinApi
+        [DllImport("user32.dll")]
+        private static extern bool PrintWindow(IntPtr hWnd, IntPtr hdcBlt, int nFlags);
+        #endregion
+
+        public static Bitmap PrintWindow(IntPtr hwnd)
+        {
+            var rect = Window.Bounds;
+
+            Bitmap bmp = new Bitmap(rect.Width, rect.Height, PixelFormat.Format32bppArgb);
+            Graphics gfx = Graphics.FromImage(bmp);
+            IntPtr hdc = gfx.GetHdc();
+
+            PrintWindow(hwnd, hdc, 0);
+
+            gfx.ReleaseHdc(hdc);
+            gfx.Dispose();
+
+            return bmp;
+        }
     }
 }
