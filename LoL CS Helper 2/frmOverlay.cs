c1879393828ddab670b80633436516baa25ca7f1
Pipe <pipernene@gmail.com>
2017-11-10 18:04:28 +0100

    Create overlay form

---
+++ LoL CS Helper 2/frmOverlay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoL_CS_Helper_2
+{
+    public partial class frmOverlay : Form
+    {
+        public frmOverlay()
+        {
+            InitializeComponent();
+        }
+    }
+}
